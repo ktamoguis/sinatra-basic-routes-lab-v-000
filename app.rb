9763896597b073f410ab260ed0775751c0f9636b
Karl Amoguis <ktamoguis@gmail.com>
2018-06-16 07:43:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,8 +6,16 @@ class App < Sinatra::Base
       "Hello, World!"
   end
 
-  get '/' do
+  get '/name' do
       "Hello, World!"
   end
-  
+
+  get '/hometown' do
+      "Hello, World!"
+  end
+
+  get '/favorite-song' do
+      "Hello, World!"
+  end
+
 end
